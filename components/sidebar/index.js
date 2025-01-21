2716a482ab69e14482d18d7914745e2744dd6fbc
Daniele <danieleformisano5@gmail.com>
2025-01-21 12:13:51 +0100

    fix: aggiustati alcune parte del codice js, aggiunti commenti

--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -1,16 +1,24 @@
+//Qui inizia la parte JavaScript dedicata all'apparizione del tooltip crea una nuova playlist
+
 const $buttonPlus = document.querySelector(".button_plus");
 
+//Evento click che aggiunge e rimuove, se l'elemento già è presente, l'elemento HTML che contiene il button-new-playlist
+
 $buttonPlus.addEventListener("click", function (event) {
-  const $existingTooltip = document.querySelector(
-    ".main__button-new-playlist__container"
-  );
+  const $existingTooltip = document.querySelector(".js-button-new-playlist");
+
+  //Verifica l'esistenza dell'elemento HTML, in caso lo rimuove
+
   if ($existingTooltip) {
     $existingTooltip.remove();
     return;
   }
+
+  //Inserisce il nuovo elemento HTML al nodo più vicino, quello di buttoPlus
+
   $buttonPlus.insertAdjacentHTML(
     "beforeend", // Puoi scegliere dove posizionare il nuovo contenuto
-    `<div class="main__button-new-playlist__container">
+    `<div class="main__button-new-playlist__container js-button-new-playlist">
         <div class="button-new-playlist__container">
           <button class="button-new-playlist">
             <svg class="song-note">
@@ -24,19 +32,21 @@ $buttonPlus.addEventListener("click", function (event) {
       </div>`
   );
   document.addEventListener("click", handleOutsideClick);
+
+  $existingTooltip.addEventListener("click", () => {});
 });
 
+//Funzione per gestire il click all'esterno del componente che è stato appena creato
+
 function handleOutsideClick(event) {
-  const $tooltip = document.querySelector(
-    ".main__button-new-playlist__container"
-  );
+  const $tooltip = document.querySelector(".js-button-new-playlist");
 
   // Se non c'è il tooltip, non fa nulla
   if (!$tooltip) return;
 
   // Controlla se il click è avvenuto fuori dal tooltip o dal bottone
   if (
-    !event.target.closest(".main__button-new-playlist__container") &&
+    !event.target.closest(".js-button-new-playlist") &&
     !event.target.closest(".button_plus")
   ) {
     $tooltip.remove(); // Rimuove il tooltip
