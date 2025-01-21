09383a0c148859165631cb9ce7434ed353d5b3f3
Daniele <danieleformisano5@gmail.com>
2025-01-21 12:31:59 +0100

    Fix: corretti errori di classe

--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -5,7 +5,9 @@ const $buttonPlus = document.querySelector(".button_plus");
 //Evento click che aggiunge e rimuove, se l'elemento già è presente, l'elemento HTML che contiene il button-new-playlist
 
 $buttonPlus.addEventListener("click", function (event) {
-  const $existingTooltip = document.querySelector(".js-button-new-playlist");
+  const $existingTooltip = document.querySelector(
+    ".main__button-new-playlist__container"
+  );
 
   //Verifica l'esistenza dell'elemento HTML, in caso lo rimuove
 
@@ -18,7 +20,7 @@ $buttonPlus.addEventListener("click", function (event) {
 
   $buttonPlus.insertAdjacentHTML(
     "beforeend", // Puoi scegliere dove posizionare il nuovo contenuto
-    `<div class="main__button-new-playlist__container js-button-new-playlist">
+    `<div class="main__button-new-playlist__container">
         <div class="button-new-playlist__container">
           <button class="button-new-playlist">
             <svg class="song-note">
@@ -39,14 +41,16 @@ $buttonPlus.addEventListener("click", function (event) {
 //Funzione per gestire il click all'esterno del componente che è stato appena creato
 
 function handleOutsideClick(event) {
-  const $tooltip = document.querySelector(".js-button-new-playlist");
+  const $tooltip = document.querySelector(
+    ".main__button-new-playlist__container"
+  );
 
   // Se non c'è il tooltip, non fa nulla
   if (!$tooltip) return;
 
   // Controlla se il click è avvenuto fuori dal tooltip o dal bottone
   if (
-    !event.target.closest(".js-button-new-playlist") &&
+    !event.target.closest(".main__button-new-playlist__container") &&
     !event.target.closest(".button_plus")
   ) {
     $tooltip.remove(); // Rimuove il tooltip
